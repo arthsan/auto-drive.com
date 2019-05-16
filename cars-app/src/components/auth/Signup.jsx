48db55d91e9b4c523c4cc82e9d2c2102b074dd77
Arthur <arthur_sbd@hotmail.com>
2019-05-16 15:33:25 -0300

    login

--- cars-app/src/components/auth/Signup.jsx
+++ cars-app/src/components/auth/Signup.jsx
@@ -1,11 +1,15 @@
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import React, { Component } from 'react';
 import AuthService from './auth-service';
 
 class Signup extends Component {
   constructor(props){
     super(props);
-    this.state = { username: '', password: '' };
+    this.state = {
+      username: '',
+      password: '',
+      redirect: false,
+    };
     this.service = new AuthService();
   }
 
@@ -22,7 +26,11 @@ class Signup extends Component {
             username: "", 
             password: "",
         });
-        this.props.getTheUser(response)
+        this.setState({
+          redirect: !this.state.redirect,
+        }, () => {
+          this.props.getUser(response);
+        });
     })
     .catch( error => console.log(error) )
   }
@@ -33,25 +41,30 @@ class Signup extends Component {
   }
 
   render(){
-    console.log(this.props)
-    return(
-      <div>
-        <form onSubmit={this.handleFormSubmit}>
-          <label>Username:</label>
-          <input type="text" name="username" value={this.state.username} onChange={ e => this.handleChange(e)}/>
-          
-          <label>Password:</label>
-          <textarea name="password" value={this.state.password} onChange={ e => this.handleChange(e)} />
-          
-          <input type="submit" value="Signup" />
-        </form>
-  
-        <p>Already have account? 
-            <Link to={"/"}> Login</Link>
-        </p>
-  
-      </div>
-    )
+    if (this.state.redirect) {
+      return(
+        <Redirect to="/" />
+      );
+    } else {
+      return(
+        <div>
+          <form onSubmit={this.handleFormSubmit}>
+            <label>Username:</label>
+            <input type="text" name="username" value={this.state.username} onChange={ e => this.handleChange(e)}/>
+            
+            <label>Password:</label>
+            <textarea name="password" value={this.state.password} onChange={ e => this.handleChange(e)} />
+            
+            <input type="submit" value="Signup" />
+          </form>
+    
+          <p>Already have account? 
+              <Link to={"/"}> Login</Link>
+          </p>
+    
+        </div>
+      );
+    }
   }
 }
 
