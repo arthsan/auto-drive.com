7ff8ecf249265974afdbb5c631d8083b1b893579
Arthur <arthur_sbd@hotmail.com>
2019-05-20 12:12:45 -0300

    mergeheroku

---
+++ cars-server/public/precache-manifest.211220d6df6442a0d5c28a376d84ceac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc76fff10eb8ce4b43a1bd4069d703c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a766121642473271b8d4",
+    "url": "/static/css/main.21530339.chunk.css"
+  },
+  {
+    "revision": "11835459f327ec826a77",
+    "url": "/static/js/2.408aabd1.chunk.js"
+  },
+  {
+    "revision": "a766121642473271b8d4",
+    "url": "/static/js/main.6411d494.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
