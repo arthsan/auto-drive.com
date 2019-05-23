b05ef39368cb7b743b4698c780140b68e21e53c7
Arthur <arthur_sbd@hotmail.com>
2019-05-23 17:22:38 -0300

    finish

--- cars-server/public/precache-manifest.d01b4d3973ff4760f1f03f030f8f10f9.js
+++ cars-server/public/precache-manifest.d01b4d3973ff4760f1f03f030f8f10f9.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dddf7b8809d2e020c692d3c7c92c0356",
+    "revision": "fb6018a070d16cda647572129690d50e",
     "url": "/index.html"
   },
   {
-    "revision": "dac606c7851585927254",
-    "url": "/static/css/main.6f84975f.chunk.css"
+    "revision": "2349a7c101c5d8cd042a",
+    "url": "/static/css/main.a15ea2cf.chunk.css"
   },
   {
     "revision": "adbca29b917168212a5b",
     "url": "/static/js/2.fce22d32.chunk.js"
   },
   {
-    "revision": "dac606c7851585927254",
-    "url": "/static/js/main.6062fb34.chunk.js"
+    "revision": "2349a7c101c5d8cd042a",
+    "url": "/static/js/main.2fe8c2f6.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
